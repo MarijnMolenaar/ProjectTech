82e3f778e02f7d28623d8228f93041b606f3c35f
MarijnMolenaar <marijnmolenaar00@gmail.com>
2022-02-27 17:51:57 +0100

    commit week 2

--- mijnbestand.js
+++ mijnbestand.js
@@ -1,11 +1,7 @@
+//Routes
+
 const express = require('express')
 const app = express()
-
-
-// respond with "hello world" when a GET request is made to the homepage
-//app.get('/', (req, res) => {
-//  res.send('hello world')
-//})
 
 app.get('/', (req, res) => {
   res.send('root')
@@ -27,21 +23,13 @@ app.get('/pug', (req, res) => {
   res.send('pug')
 })
 
-app.get('/index', (req, res) => {
-  res.send('file:///C:/Users/marij/myapp/index.html')
+app.get('/index.html', (req, res) => {
+  res.send('index.html')
 })
-
 
 app.listen(8080)
 
 app.use(express.static('content'))
 
+// Javascript logic
 
-
-
-
-//var http = require('http');
- //http.createServer(function (req, res) {
- //  res.writeHead(200, {'Content-Type': 'text/html'});
- //  res.end('Het werkt wtf!');
-// }).listen(8080);
